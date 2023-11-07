3c7765b79b518fe75ac16cdc45343374e3a8b265
Roman Makhnev <romanmahnef777@gmail.com>
2023-11-07 18:32:17 +0400

    1

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,10 +1,18 @@
 import unittest
-from utils import arrs
+from utils.arrs import get, my_slice
 
 
-class TestCal(unittest.TestCase):
+class TestExample(unittest.TestCase):
+
     def test_get(self):
-        self.assertEqual([1, 2, 3],[1, 2, 3])
-    def test_slice(self):
-        self.assertEqual([1, 2, 3],  [1, 2, 3])
+        self.assertEqual(get([1, 2, 3], 1), 2)
+        self.assertEqual(get([1, 2, 3], 5), None)
 
+    def test_my_slice(self):
+        self.assertEqual(my_slice([1, 2, 3, 4, 5], 1, 3), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3, 4, 5], 2), [3, 4, 5])
+        self.assertEqual(my_slice([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+
+if __name__ == '__main__':
+    unittest.main()
